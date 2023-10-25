fe1100ad9918a0be33f0c4ac3e31108cd2b74d0e
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-25 19:12:05 -0400

    Adds the sky and ground surfaces.

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('runner-game')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('White')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/Ground.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface, (200, 100))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
     pygame.display.update()
     clock.tick(60)
