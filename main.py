ecb9dd28bd41cb939ec9f9f2184190c366dff4cb
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-25 16:20:46 -0400

    Sets up the game loop.

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('runner-game')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
