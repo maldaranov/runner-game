06cc9ee5a9e859984ac895e8f8be3be757c52752
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-25 21:35:25 -0400

    Implements player jump functionality.

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import pygame
 from sys import exit
 
-# initial setup and variables
+# init
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('runner-game')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-# general surfaces
-sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
-ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
-text_surface = test_font.render('Look, it\'s a snail!', False, 'Black')
+# score-init
+score_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+score_surf = score_font.render('Look, it\'s a snail!', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
-# entity surfaces
+# background-init
+sky_surf = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surf = pygame.image.load('graphics/Ground.png').convert_alpha()
+
+# player-init
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80,300))
+player_gravity = 0
 
-# entity rectangles
-player_rect = player_surface.get_rect(midbottom = (80, 300))
-snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+# snail-init
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (600,300))
+
 
 # game loop
 while True:
@@ -27,21 +32,26 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+    
+    # background-draw
+    screen.blit(sky_surf, (0,0))
+    screen.blit(ground_surf, (0,300))
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300, 50))
+    # score-draw
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
+    screen.blit(score_surf, score_rect)
 
-    screen.blit(snail_surface, snail_rect)
+    # snail-draw
     snail_rect.left = (snail_rect.left - 5) % 800
+    screen.blit(snail_surface, snail_rect)
+
+    # player-draw
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surface, player_rect)
-
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
