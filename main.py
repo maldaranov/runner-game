0584f6b7dd706e1132505ad1b95ebc541865fc0c
Maxim Aldaranov <maxim.aldaranov@gmail.com>
2023-10-25 20:00:16 -0400

    Adds the snail rectangle.

--- main.py
+++ main.py
@@ -8,20 +8,18 @@ pygame.display.set_caption('runner-game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-# surfaces
+# general surfaces
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/Ground.png').convert_alpha()
 text_surface = test_font.render('Look, it\'s a snail!', False, 'Black')
+
+# entity surfaces
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 
-# rectangles
+# entity rectangles
 player_rect = player_surface.get_rect(midbottom = (80, 300))
-
-# entities
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-
-# coordinates
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(midbottom = (600, 300))
 
 # game loop
 while True:
@@ -33,9 +31,10 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    screen.blit(snail_surface, (snail_x_pos, 250))
+
+    screen.blit(snail_surface, snail_rect)
+    snail_rect.left = (snail_rect.left - 5) % 800
     screen.blit(player_surface, player_rect)
-    snail_x_pos = (snail_x_pos - 4) % 800
 
     pygame.display.update()
     clock.tick(60)
